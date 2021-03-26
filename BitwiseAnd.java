d3bb9117ec3fd46600433e595540051ff96dacec
Pummy12 <mepummyjha@gmail.com>
2021-03-26 22:34:01 +0530

    Bitwise And Operator JAVA

---
+++ BitwiseAnd.java
@@ -0,0 +1,10 @@
+package operator;
+public class BitwiseAnd {
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = 6;
+        int c = a & b;
+        System.out.println(c);
+    } 
+}
