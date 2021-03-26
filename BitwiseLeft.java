a82e0cf6c791b562f3174575185fed8ee3012c25
Pummy12 <mepummyjha@gmail.com>
2021-03-26 22:32:37 +0530

    Bitwise Left Operator JAVA

---
+++ BitwiseLeft.java
@@ -0,0 +1,11 @@
+package operator;
+public class BitwiseLeft {
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = 13;
+        int c = b << 2;
+        System.out.println(c);
+    }
+    
+}
