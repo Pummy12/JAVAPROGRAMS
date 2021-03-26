96c3d5f1ecf428d08644116e383c9ba706b29506
Pummy12 <mepummyjha@gmail.com>
2021-03-26 22:30:19 +0530

    Biwise Right Operator JAVA

---
+++ BitwiseRight.java
@@ -0,0 +1,11 @@
+package operator;
+public class BitwiseRight {
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=6;
+        int c= b >> 2;
+        System.out.println(c);
+    }
+    
+}
