78039724aa6fdea7e2f56f263deb36199c15a5d2
Pummy12 <mepummyjha@gmail.com>
2021-04-05 00:05:24 +0530

    Prime no. or not

---
+++ Prime.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Prime {
+public static void main(String args[])
+{
+    int n;
+    int x=0;
+    Scanner sc=new Scanner(System.in) ;
+    System.out.println("Enter a number");
+     n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+     {
+         if(n%i==0)
+         x++;
+     }
+     if(x==2)
+     System.out.println("Prime number");
+     else
+     System.out.println("Not a prime number");
+}    
+}
