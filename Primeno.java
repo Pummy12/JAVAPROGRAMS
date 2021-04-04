5d93d1ea1faee8fc65d4cb8c02609dac621f9ae2
Pummy12 <mepummyjha@gmail.com>
2021-04-05 00:03:35 +0530

    Prime no. or not

---
+++ Primeno.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Primeno {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER A NUMBER");
+        int n=sc.nextInt();
+        boolean isPrime=true;
+        for(int i=2; i*i <=n;i++){
+            if(n%i ==0)
+             {
+                 isPrime = false;
+                 break;
+            }
+        }
+        if(n<2)
+        isPrime = false;
+        System.out.println("isprime "+isPrime);
+    }
+    
+}
