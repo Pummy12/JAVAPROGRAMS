c9ca6488dc60e3549ef58b5c47209e79ea037dca
Pummy12 <mepummyjha@gmail.com>
2021-04-05 13:22:19 +0530

    Pattern Program in JAVA using * and space

---
+++ Pattern6.java
@@ -0,0 +1,37 @@
+ /*
+    *    
+   * *   
+  * * *
+ * * * *
+* * * * *
+
+*/ 
+
+
+
+
+
+public class Pattern6 {
+    
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++)
+        {
+            int k=1;
+            for(int j=1;j<=9;j++)
+            {
+                if(j>=6-i && j<=i+4 && k==1){
+                System.out.print("*");
+                k=0;
+                }
+                else 
+                {
+                    System.out.print(" ");
+                    k=1;  
+                }
+                
+            }
+            System.out.println();
+
+        }
+    }
+}
