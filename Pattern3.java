610d51fbe280286514fd96dc378d260d3462019b
Pummy12 <mepummyjha@gmail.com>
2021-04-05 13:19:08 +0530

    Pattern Program in JAVA

---
+++ Pattern3.java
@@ -0,0 +1,25 @@
+// Pattern triangle
+/*      *
+       **
+      ***
+     ****
+    ***** 
+    */   
+
+public class Pattern3
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(j>=6-i)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println(); 
+        }
+    }
+}
