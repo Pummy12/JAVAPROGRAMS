04c88ba925de95a19f14f4b3218000c0ca6b4dea
Pummy12 <mepummyjha@gmail.com>
2021-04-06 13:19:12 +0530

    Pattern using * to make X

---
+++ Pattern8.java
@@ -0,0 +1,28 @@
+/* 
+   *   *
+    * * 
+     *
+    * *
+   *   *
+
+*/
+
+
+public class Pattern8 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(i==j || j==6-i)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
