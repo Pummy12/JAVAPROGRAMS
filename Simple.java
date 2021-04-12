42b2d2b04ef38a95b71fa3e36cb2d35fbb75033a
Pummy12 <mepummyjha@gmail.com>
2021-04-12 21:49:48 +0530

    Instance, static and local variable

---
+++ Simple.java
@@ -0,0 +1,12 @@
+class Simple
+{
+int m=20; // instance variables
+static int n=30;// static variables
+public static void main(String[] args) // main method
+{
+int a=10; // local variable
+int b=10;
+int c=a+b;
+System.out.println(c);
+}
+}
